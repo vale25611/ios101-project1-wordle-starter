d9e370c0415cb44dab2a83c812959b8836bb49ec
vale25611 <vquiroga25611@gmail.com>
2025-09-24 15:43:14 -0400

    Adding app files

--- Wordle/KeyboardCell.swift
+++ Wordle/KeyboardCell.swift
@@ -23,6 +23,7 @@ class KeyboardCell: UICollectionViewCell {
   func configure(with string: String) {
     labelContainerView.layer.cornerRadius = 4.0
     labelContainerView.backgroundColor = .gray
+
     /* Exercise 2:
       1. Assign the argument `string` to the `self.string` private property (see line 14)
       2. Change the text of the label to the value of the passed in string
@@ -30,15 +31,17 @@ class KeyboardCell: UICollectionViewCell {
       Checkpoint: After finishing this exercise, you should now be able to see the keyboard laid out on the screen! If it's not working, check your work on exercises 1 & 2.
     */
     // START YOUR CODE HERE
-    // ...
+    self.string = string
+      label.text = string
     // END YOUR CODE HERE
   }
 
   // Exercise 5: Call the `didSelectString` closure and pass in the string this cell holds (see line 14)
   // Checkpoint: After finishing this exercise, you should now be able to tap on a keyboard cell and have the letter shown on the board! If it's not working, check your work on exercises 3,4, and 5.
+
   @objc private func didTapString() {
     // START YOUR CODE HERE
-    // ...
+      didSelectString?(string)
     // END YOUR CODE HERE
   }
 }
--- Wordle/ViewController.swift
+++ Wordle/ViewController.swift
@@ -29,8 +29,15 @@ class ViewController: UIViewController {
       If the string is equal to the `DELETE_KEY` constant (see Constants.swift), then call the `deleteLastCharacter` method of `boardController`.
       Else, it should use the `enter` method of `boardController` and pass in the selected string as the argument.
      */
-    // START YOUR CODE HERE
-    // ...
-    // END YOUR CODE HERE
+      keyboardController.didSelectString = {
+          [weak self] str in guard let self = self else { return }
+          if str == DELETE_KEY {
+              self.boardController.deleteLastCharacter()
+          } else {
+              self.boardController.enter(str)
+          }
+
+      }
+ 
   }
 }
